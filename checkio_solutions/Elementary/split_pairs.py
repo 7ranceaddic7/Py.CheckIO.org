2e2a539319d9da2297d2e64dc49151c844250183
Scott <4062616+7ranceaddic7@users.noreply.github.com>
2020-10-21 16:22:33 -0700

    a

--- checkio_solutions/Elementary/split_pairs.py
+++ checkio_solutions/Elementary/split_pairs.py
@@ -1,29 +1,27 @@
-#!/usr/bin/env checkio --domain=py run split-pairs
-
 # Split the string into pairs of two characters. If the string contains an odd number of characters, then the missing second character of the final pair should be replaced with an underscore ('_').
-# 
-# Input:A string.
-# 
-# Output:An iterable of strings.
-# 
-# Precondition:0<=len(str)<=100
-# 
-# 
-# END_DESC
 
 def split_pairs(a):
-    # your code here
-    return None
+    list(a)
+    print(type(a))
+    print(a)
+    # if len(a) % 2 == 0:
+    #     half = len(a) / 2
+    #     print(half)
+    # listA = a[:half]
+    # listB = a[half:]
+    # return listA, listB
 
 
-if __name__ == '__main__':
-    print("Example:")
+# if __name__ == '__main__':
+    # print("Example:")
     print(list(split_pairs('abcd')))
 
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert list(split_pairs('abcd')) == ['ab', 'cd']
-    assert list(split_pairs('abc')) == ['ab', 'c_']
-    assert list(split_pairs('abcdf')) == ['ab', 'cd', 'f_']
-    assert list(split_pairs('a')) == ['a_']
-    assert list(split_pairs('')) == []
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+    # # These "asserts" are used for self-checking and not for an auto-testing
+    # assert list(split_pairs('abcd')) == ['ab', 'cd']
+    # assert list(split_pairs('abc')) == ['ab', 'c_']
+    # assert list(split_pairs('abcdf')) == ['ab', 'cd', 'f_']
+    # assert list(split_pairs('a')) == ['a_']
+    # assert list(split_pairs('')) == []
+    # print("Coding complete? Click 'Check' to earn cool rewards!")
+
+# TOGETHER800K
